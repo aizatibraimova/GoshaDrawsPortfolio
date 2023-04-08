f109ee1debdb426838bdf10b95ca756e9dcf0371
Aizat I <ibraimova.as@gmail.com>
2023-04-08 12:43:11 -0500

    Portfolio page, gallery carousel inside modal

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 $('#videoCarousel').carousel({
     interval: 10000
   })
+
+
+  // function typeText() {
+  //   const element = document.querySelector('.typingText');
+  //   const text = element.textContent;
+  //   let i = 0;
+  //   let speed = 100;
+  //   element.textContent = '';
+  
+  //   function type() {
+  //     if (i < text.length) {
+  //       element.textContent += text.charAt(i);
+  //       i++;
+  //       setTimeout(type, speed);
+  //     }
+  //   }
+  //   type();
+  // }
+  
